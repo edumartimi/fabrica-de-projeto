99199d28dd7011f6c7fddacabdf981a99e36bf46
edumartimi <eduardomartimiano@hotmail.com>
2021-10-12 18:43:04 -0300

    adição de sons e animações
    adicionado a animação do personagem, com som dos passos

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,6 +9,8 @@ public class Player : MonoBehaviour
     private Rigidbody2D fisica;
     public int velocidade;
     private bool correr;
+    private float tmpcorrida;
+    public float estamina;
 
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -27,20 +29,30 @@ public class Player : MonoBehaviour
     {
         fisica = GetComponent<Rigidbody2D>();
     }
-
+        
     private void FixedUpdate() 
     {
+        /*
         Vector3 diferrence = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         diferrence.Normalize();
 
         float rotationZ = Mathf.Atan2(diferrence.y, diferrence.x) * Mathf.Rad2Deg;
 
         transform.rotation = Quaternion.Euler(0f,0f, rotationZ);
+        */
 
         if (!correr)
         {
-            Vector2 andar = new Vector2(Input.GetAxis("Horizontal") * velocidade, Input.GetAxis("Vertical") * velocidade);
-            fisica.velocity = andar;
+            if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+            {
+                Vector2 andar = new Vector2(Input.GetAxis("Horizontal") * velocidade, Input.GetAxis("Vertical") * velocidade);
+                fisica.velocity = andar;
+            }
+            else 
+            {
+                Vector2 parado = new Vector2(0, 0);
+                fisica.velocity = parado;
+            }
         }
         if (correr)
         {
@@ -51,16 +63,37 @@ public class Player : MonoBehaviour
 
     }
 
-    private void Update() { 
-   
-        if (Input.GetKeyDown(KeyCode.LeftShift)) 
+    private void Update() {
+        if (Input.GetKey(KeyCode.LeftShift)) 
         {
-            correr = true;
+                correr = true;
         }
 
         if (Input.GetKeyUp(KeyCode.LeftShift))
         {
             correr = false;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            fisica.rotation = 180;
+        }
+        
+        
+        if(Input.GetKey(KeyCode.D))
+        {
+            fisica.rotation = 0;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            fisica.rotation = 90;
+        }
+
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            fisica.rotation = 270;
         }
 
     }
