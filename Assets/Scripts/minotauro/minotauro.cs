6f739a4644f613596be1845f1b18fc23b9e6ca32
edumartimi <eduardomartimiano@hotmail.com>
2021-11-05 12:15:18 -0300

    adição de tochas e parede nova
    tocha

---
+++ Assets/Scripts/minotauro/minotauro.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class minotauro : MonoBehaviour
+{
+    private Rigidbody2D fisica;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fisica = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
